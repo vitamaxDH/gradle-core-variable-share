fd0f40dc8fde7d4e81cc02bf25ecd53aa163c7b7
Max <hellonewtry@gmail.com>
2022-12-03 01:03:32 +0900

    chore: remove unneccessary codes

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,17 +2,6 @@ plugins {
     id("java")
 }
 
-group = "com.vitamax"
-version = "1.0"
-
-dependencies{
-    implementation(project(":shared"))
-}
-
-tasks.withType(JavaCompile::class.java) {
-    options.encoding = "UTF-8"
-}
-
 tasks.create("api-task") {
     dependsOn(":shared:shared-task")
     doFirst {
